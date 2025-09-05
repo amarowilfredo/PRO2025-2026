f1ed9e54573534c466c296499e176c810b311bcf
Wilfredo2025 <Wilfredo2025@DESKTOP-ECQEJ0I>
2025-09-05 18:58:18 +0100

    Git para programación (PRO) 2025-2026

---
+++ src/main/java/com/mycompany/programacion_2025_2026/Programacion_2025_2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacion_2025_2026;
+
+/**
+ *
+ * @author Wilfredo2025
+ */
+public class Programacion_2025_2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
